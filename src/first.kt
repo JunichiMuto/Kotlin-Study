26302299130e3698e452e87af56b8c8f1faf2fdb
JunichiMuto <JunichiMuto@users.noreply.github.com>
2019-10-27 17:39:31 +0900

    一旦nullableとボクシングまで

---
+++ src/first.kt
@@ -0,0 +1,4 @@
+fun main(args:Array<String>){
+    var name:String = "山田"
+    println("こんにちは、${name}さん！")
+}
---
+++ src/str_raw2.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var msg = """
+        !「Kotlin」は、「コトリン」と読みます。
+        !JetBrainsが階発意したオブジェクト指向言語です。
+        !Android Studio 3でサポートされています。
+    """.trimMargin("!")
+    println(msg)
+}
